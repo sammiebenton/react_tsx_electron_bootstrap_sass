5c0b96155e782378d7e5d261f031d3b276186da5
Sammie <info@sammiebenton.com>
2020-10-30 11:46:40 +0000

    initial boilerplate setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { hot } from 'react-hot-loader'
+import React from 'react'
+
+const App = () => {
+  return(
+    <div className="body">
+      <h1>Hello World</h1>
+    </div>
+  )
+};
+
+export default hot(module)(App)
